de3bb861c53207f50deab3b0be30907689ce2817
juanillo22 <juanillotabernero2yt@gmail.com>
2023-04-10 10:56:27 +0000

    act

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -1,10 +1,22 @@
+<div>
 <nav>
-    <br>
-    <ul>
-        <li style="font-size: 25px; "><a href="{{route('home')}}" style="">Home</a></li>
-        <li style="font-size: 25px;"><a href="{{route('posts.index')}}">Blog</a></li>
-        <li style="font-size: 25px;"><a href="{{route('about')}}">About</a></li>
-        <li style="font-size: 25px;"><a href="{{route('contact')}}">Contacto</a></li>
-        <li style="font-size: 25px;"><a href="{{route('registro')}}">Registrarse</a></li>
-    </ul>  
+    <br>  
+        <li><a href="{{route('home')}}" style="">Home</a></li>
+        <li><a href="{{route('posts.index')}}">Blog</a></li>
+        <li><a href="{{route('about')}}">About</a></li>
+        <li><a href="{{route('contact')}}">Contacto</a></li>
+        
+        
+        @auth
+        <form action="{{route('logout')}}" method="POST">
+        @csrf
+        <button class="btn">Logout</button>
+        </form>
+        @endauth
+
+        @guest
+        <li class="login"><a href="{{route('login')}}">Login</a></li>
+        <li class="registro"><a href="{{route('registro')}}">Registrarse</a></li>
+        @endguest
 </nav>
+</div>
